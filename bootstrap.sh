44a9901ff2d593cc16a4266001037a790110f484
Luís Martinho <lmartinho@gmail.com>
2013-04-12 14:38:08 +0100

    much better bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+rm -rf $HOME/.vim
+ln -s $HOME/.dotfiles/etc/vim $HOME/.vim
+rm -rf $HOME/.vimrc
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
--- etc/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-rm $HOME/.vim
-ln -s $HOME/workspace/dot-files/etc/vim $HOME/.vim
-rm $HOME/.vimrc
-ln -s $HOME/.vim/vimrc $HOME/.vimrc
